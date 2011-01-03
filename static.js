89b37195733bdd4ca8970e414bf6414d4bf65c4d
Tim Caswell <tim@creationix.com>
2011-01-03 22:11:10 +0000

    Add Range query support so media files can stream

--- static.js
+++ static.js
@@ -45,10 +45,34 @@ module.exports = function setup(mount, root, index) {
         res.end();
         return;
       }
-      headers["Content-Length"] = stat.size;
+      var start = 0;
+      var end = stat.size - 1;
+      var code = 200;
+      if (req.headers.range) {
+        var p = req.headers.range.indexOf('=');
+        var parts = req.headers.range.substr(p + 1).split('-');
+        if (parts[0].length) { 
+          start = parseInt(parts[0], 10);
+          if (parts[1].length) {
+            end = parseInt(parts[1], 10);
+          }
+        } else {
+          if (parts[1].length) {
+            start = end + 1 - parseInt(parts[1], 10);
+          }
+        }
+        if (end < start || start < 0 || end >= stat.size) {
+          res.writeHead(416, headers);
+          res.end();
+          return;
+        }
+        code = 206;
+        headers["Content-Range"] = "bytes " + start + "-" + end + "/" + stat.size;
+      }
+      headers["Content-Length"] = end - start + 1;
       headers["Content-Type"] = getMime(path);
-      var stream = Fs.createReadStream(path);
-      res.writeHead(200, headers);
+      var stream = Fs.createReadStream(path, {start: start, end: end});
+      res.writeHead(code, headers);
       stream.pipe(res);
       stream.on('error', next);
     }
