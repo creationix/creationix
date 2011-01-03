ba87edd1594b401a47a25262010724e212ea97dd
Tim Caswell <tim@creationix.com>
2011-01-03 22:10:24 +0000

    Add more headers to every output

--- log.js
+++ log.js
@@ -4,9 +4,11 @@ module.exports = function setup(special) {
     "Content-Type": true,
     "Location": true,
     "Content-Length": true,
+    "Content-Range": true,
   };
   return function handle(req, res, next) {
     var writeHead = res.writeHead;
+    var start = Date.now();
     res.writeHead = function (code, headers) {
       var extra = [];
       Object.keys(headers).forEach(function (key) {
@@ -17,6 +19,8 @@ module.exports = function setup(special) {
       if (!headers.hasOwnProperty('Date')) {
         headers.Date = (new Date()).toUTCString();
       }
+      headers.Server = "NodeJS " + process.version;
+      headers["X-Runtime"] = Date.now() - start;
       console.log("%s %s %s %s", req.method, req.url, code, extra.join(" "));
       res.writeHead = writeHead;
       res.writeHead(code, headers);
