9893d54d59c7264b7f65e4fa8ebf72f0936a1bf1
Tim Caswell <tim.caswell@palm.com>
2011-02-18 10:29:05 -0800

    Remove modules hash from package.json since npm no longer supports it
    Change-Id: I00b8a236af8c6475aafeb53cf9af959487d85454

---
+++ vhost.js
@@ -0,0 +1,11 @@
+
+
+module.exports = function setup(domain, handler) {
+  return function handle(req, res, next) {
+    if (req.headers.host === domain) { 
+      handler(req, res, next);
+    } else {
+      next();
+    }
+  };
+};
