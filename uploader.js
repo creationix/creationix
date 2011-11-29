d543c8ca1791633f859c79a84422cc75898bc6f8
Tim Caswell <tim@creationix.com>
2011-11-29 09:51:28 -0600

    Fix uploader, add deleter and bump version

--- uploader.js
+++ uploader.js
@@ -1,11 +1,17 @@
 var FS = require('fs'),
-    Url = require('url');
+    Url = require('url'),
+    Path = require('path');
 
 module.exports = function setup(mount, root) {
   return function handle(req, res, next) {
     if (req.method !== "PUT") return next();
     if (!req.uri) { req.uri = Url.parse(req.url); }
-    var stream = FS.createWriteStream(req.uri.pathname);
+    var path = unescape(req.uri.pathname).replace(/\.\.+/g, '.');
+    if (!path || path.substr(0, mount.length) !== mount) {
+      return next();
+    }
+    path = Path.join(root, path.substr(mount.length));
+    var stream = FS.createWriteStream(path);
     stream.on('error', next);
     req.pipe(stream);
     req.on('end', function () {
