d37790a6fcb856e7a7cdf24750f82416ba5b0652
Tim Caswell <tim@creationix.com>
2011-12-29 15:49:43 -0600

    Update vhost to use a table for all vhosts

--- vhost.js
+++ vhost.js
@@ -1,13 +1,17 @@
 
-module.exports = function setup(domain, handler) {
+module.exports = function setup(domainHandlers, defaultHandler) {
+  var domains = Object.keys(domainHandlers);
+  var length = domains.length;
   return function handle(req, res, next) {
     var host = req.headers.host;
-    var i = host.indexOf(":");
-    if (i >= 0) host = host.substr(0, i);
-    if (host === domain) {
-      handler(req, res, next);
-    } else {
-      next();
+    var p = host.indexOf(":");
+    if (p >= 0) host = host.substr(0, p);
+    for (var i = 0; i < length; i++) {
+      var domain = domains[i];
+      if (domain !== host) continue;
+      return domainHandlers[domain](req, res, next);
     }
+    if (defaultHandler) return defaultHandler(req, res, next);
+    next();
   };
 };
