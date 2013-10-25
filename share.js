1e8fa036b9c75d5422d6ffd9cc84d63fb134fa0f
Tim Caswell <tim@creationix.com>
2013-10-24 20:59:36 -0500

    Remove all unused code and update share to use send

--- share.js
+++ share.js
@@ -1,25 +1,29 @@
 #!/usr/bin/env node
 
-var Http = require('http'),
-    Stack = require('stack'),
-    Creationix = require('creationix');
-var path = process.cwd();
+var http = require('http');
+var send = require('send');
+var url = require('url');
+var root = process.argv[2] || process.cwd();
 
-var handler = Stack(
-  Creationix.log(),
-  Creationix.static('/', path, 'index.html'),
-  Creationix.indexer('/', path)
-);
+function onRequest(req, res) {
+  send(req, url.parse(req.url).pathname)
+    .root(root)
+    .pipe(res);
+}
+
 var PORT = 8080;
 function listen() {
-  try {
-    Http.createServer(handler).listen(PORT);
-    console.log("Serving %s at http://localhost:%s/", path, PORT);
-  } catch (err) {
-    // this is probably broken in node 0.5.x. Should check err.code instead.
-    if (err.errno !== 98) { throw err; }
-    PORT++;
-    listen();
-  }
+  var server = http.createServer(onRequest);
+  server.on('error', function (err) {
+    if (err.code === 'EADDRINUSE') {
+      console.log("Port %s busy, trying the next...", PORT);
+      PORT++;
+      return listen();
+    }
+    throw err;
+  });
+  server.listen(PORT, function () {
+    console.log("Serving %s at http://localhost:%s/", root, server.address().port);
+  });
 }
 listen();
